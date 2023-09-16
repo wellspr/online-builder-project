a94f12d68eea2911e2f270e842dbddc8c8fc7562
Paulo Wells <paulowells@gmail.com>
2023-09-16 10:05:20 -0300

    added a Logo component

---
+++ app/images/Logo.tsx
@@ -0,0 +1,23 @@
+const Logo = () => {
+
+    return (
+        <svg className="logo" width="450" height="450" viewBox="0 0 450 450" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g id="Cog">
+                <g id="Group 1">
+                    <circle id="internal" cx="225.138" cy="225.548" r="123.414"
+                        transform="rotate(-45 225.138 225.548)" fill="currentColor" />
+                    <rect id="d1" x="96.3102" y="129.928" width="47.2501" height="317.351" rx="15"
+                        transform="rotate(-45 96.3102 129.928)" fill="currentColor" />
+                    <rect id="d1_2" x="203.105" y="66.9964" width="47.2501" height="316.646" rx="15"
+                        fill="currentColor" />
+                    <rect id="d1_3" x="320.712" y="97.0153" width="47.2501" height="317.351" rx="15"
+                        transform="rotate(45 320.712 97.0153)" fill="currentColor" />
+                    <rect id="d1_4" x="383.643" y="200.284" width="47.2501" height="317.351" rx="15"
+                        transform="rotate(90 383.643 200.284)" fill="currentColor" />
+                </g>
+            </g>
+        </svg>
+    );
+};
+
+export default Logo;
