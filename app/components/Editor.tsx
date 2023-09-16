e8f66afb4c2a6d1d0cc04ac00f6ac7635bceb0ca
Paulo Wells <paulowells@gmail.com>
2023-09-16 10:22:26 -0300

    updated types and configs.

--- app/components/Editor.tsx
+++ app/components/Editor.tsx
@@ -1,18 +1,35 @@
-import { FC, useEffect, useState } from "react";
-import { apiHandler } from "~/apiHandler";
-import { Editor as MonacoEditor } from "@monaco-editor/react"
-import defaultCode from "~/config/defaultCode";
+// React
+import { FC, useEffect, useRef, useState } from "react";
+
+// Esbuild
 import { useBuilder } from "~/esbuild-context/BuildContext";
+
+// Monaco Editor
+import { Editor as MonacoEditor } from "@monaco-editor/react";
+import type { OnMount, OnChange, Monaco } from "@monaco-editor/react";
+import monaco, { editor } from "monaco-editor";
+
+// Locals
+import { apiHandler } from "~/apiHandler";
+import defaultCode from "~/config/defaultCode";
+import EditorLoader from "./EditorLoader";
 
 const Editor: FC = () => {
 
-    const { outputRef } = useBuilder();
+    const monacoRef = useRef<Monaco>();
+    const editorRef = useRef<editor.IStandaloneCodeEditor>();
+
+    const { outputRef, setOutputCode, setLoadingCode, loadingCode } = useBuilder();
     const [inputCode, setInputCode] = useState<string | undefined>(defaultCode);
-    const { setOutputCode } = useBuilder();
     const [err, setErr] = useState<string | null>(null);
+
+    const onClickTest = () => {
+        console.log(monacoRef.current, editorRef.current);
+    };
 
     const buildCode = () => {
         if (inputCode) {
+            setLoadingCode(true);
             apiHandler.build(inputCode)
                 .then(r => {
                     console.log("BUILD RESPONSE: ", r.data);
@@ -21,34 +38,82 @@ const Editor: FC = () => {
                         outputRef.current.contentWindow.postMessage(r.data.outputFiles[0].text, "*");
                     }
                 })
-                .catch(err => setErr(err));
+                .catch(err => setErr(err))
+                .finally(() => setLoadingCode(false));
+                
         }
     };
 
-    useEffect(() => {
-        if (err) {
-            console.log(err);
-        }
-    }, [err]);
+    const onMount: OnMount = (editor, monaco) => {
+        editorRef.current = editor;
+        monacoRef.current = monaco;
+
+        monaco.languages.typescript.typescriptDefaults.setCompilerOptions({
+            target: monaco.languages.typescript.ScriptTarget.Latest,
+            allowNonTsExtensions: true,
+            moduleResolution: monaco.languages.typescript.ModuleResolutionKind.NodeJs,
+            module: monaco.languages.typescript.ModuleKind.CommonJS,
+            noEmit: true,
+            esModuleInterop: true,
+            jsx: monaco.languages.typescript.JsxEmit.React,
+            jsxFactory: "React.createElement",
+            reactNamespace: "React",
+            allowJs: true,
+            typeRoots: ["node_modules/@types"],            
+        });
+
+        monaco.languages.typescript.typescriptDefaults.setDiagnosticsOptions({
+            noSemanticValidation: false,
+            noSyntaxValidation: false,
+        });
+
+        monaco.languages.typescript.typescriptDefaults.addExtraLib(
+            '<<react-definition-file>>',
+            `file:///node_modules/@react/types/index.d.ts`
+        );
+    };
+
+    const onChange: OnChange = (value, event) => {
+        setInputCode(value);
+    };
+
+    const options: monaco.editor.IStandaloneEditorConstructionOptions = {
+        extraEditorClassName: "custom-monaco-editor",
+        minimap: { enabled: false },
+        //fontFamily: 
+        fontSize: 12,
+        lineHeight: 1.6,
+        parameterHints: { enabled: true },
+        scrollBeyondLastLine: false,
+        showUnused: true,
+        wordWrap: "on",
+        folding: false,
+        lineNumbersMinChars: 3,
+        automaticLayout: true,
+    };
+
+    useEffect(() => { err && console.log(err) }, [err]);
 
     return (
-        <form onSubmit={e => e.preventDefault()}>
-            <MonacoEditor
-                height="15rem"
-                defaultLanguage="javascript"
-                defaultValue={inputCode}
-                onChange={(value, event) => {
-                    setInputCode(value);
-                }}
-                options={{
-                    minimap: {enabled: false}
-                }}
-            />
-            <button
-                type="submit"
-                onClick={buildCode}
-            >Submit</button>
-        </form>
+        <section className="editor" onSubmit={e => e.preventDefault()}>
+            <div className="buttons">
+                <button
+                    className="button"
+                    type="button"
+                    onClick={buildCode}
+                >Run</button>
+            </div>
+            <div className="monaco-editor-wrapper">
+                <MonacoEditor
+                    language="typescript"
+                    defaultValue={inputCode}
+                    onChange={onChange}
+                    onMount={onMount}
+                    options={options}
+                    loading={<EditorLoader />}
+                />
+            </div>
+        </section>
     );
 };
 
